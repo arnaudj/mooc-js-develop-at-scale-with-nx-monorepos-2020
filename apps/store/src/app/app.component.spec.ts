6cfec744b5e78820fbf35f8727125653c749816d
Arnaud J <github@arnaud.dev>
2022-10-20 00:17:52 +0200

    build: rm unapplicable unit test

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -20,15 +20,6 @@ describe('AppComponent', () => {
   it(`should have as title 'store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome store'
-    );
+    expect(app.title).toEqual('Board Game Hoard');
   });
 });
