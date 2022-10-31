da30d84281bcaf4bef5addd9c9cd3aa87d27e069
Arnaud J <github@arnaud.dev>
2022-10-31 23:34:43 +0100

    feat: use games API

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -13,6 +14,7 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
     BrowserModule,
     MatCardModule,
     StoreUiSharedModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
