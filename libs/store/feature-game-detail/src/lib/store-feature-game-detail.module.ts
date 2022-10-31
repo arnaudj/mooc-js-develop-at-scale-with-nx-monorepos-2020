0ffbf7ad9eb999befc8e5fd39d73c9723a563137
Arnaud J <github@arnaud.dev>
2022-10-31 16:12:30 +0100

    feat: add component game-detail to store-feature-game-detail
    nx generate @nrwl/angular:component game-detail --project=store-feature-game-detail

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { storeFeatureGameDetailRoutes } from './lib.routes';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(storeFeatureGameDetailRoutes)],
+  declarations: [GameDetailComponent],
 })
 export class StoreFeatureGameDetailModule {}
