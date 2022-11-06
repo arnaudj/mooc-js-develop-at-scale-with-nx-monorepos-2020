3e88d13f4d7962addf8d5510394e564f063e66ff
Arnaud J <github@arnaud.dev>
2022-11-06 22:01:32 +0100

    feat lab 14: add generator code
    usage: nx workspace-generator update-scope-schema

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,10 +1,45 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import {
+  Tree,
+  updateJson,
+  formatFiles,
+  ProjectConfiguration,
+  getProjects,
+} from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags.filter((tag: string) => tag.startsWith('scope:'))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+  return Array.from(new Set(allScopes));
+}
+
+function replaceScopes(content: string, scopes: string[]): string {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
+  return content.replace(
+    PATTERN,
+    `interface Schema {
+      name: string;
+      directory: ${joinScopes};
+    }`
+  );
+}
+
+export default async function (tree: Tree) {
+  const scopes = getScopes(getProjects(tree));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', (schemaJson) => {
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
+    return schemaJson;
+  });
+  const content = tree.read('tools/generators/util-lib/index.ts', 'utf-8');
+  const newContent = replaceScopes(content, scopes);
+  tree.write('tools/generators/util-lib/index.ts', newContent);
   await formatFiles(tree);
-  return () => {
-    installPackagesTask(tree);
-  };
 }
